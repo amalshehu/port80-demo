2b75d74252feb1bf95835eb4cca49ab41ff3a8e3
amalshehu <amalshehu@gmail.com>
2017-07-19 11:50:43 +0530

    Add search method

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,8 +1,22 @@
+import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';  // for debugging
+
 
 @Injectable()
 export class AppService {
+  handleError(arg0: any): any {
+    throw new Error('Method not implemented.');
+  }
 
-  constructor() { }
-
+  constructor(private http: Http) { }
+  search(link: any) {
+   return this.http.request(link)
+                    .map((res: any) => res.json())
+                    .do(data => console.log('server data:', data))  // debug
+                    .catch(this.handleError);
+  }
 }
