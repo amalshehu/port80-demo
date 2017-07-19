ab8a985854b5deec044918c08ac72d2942b41601
amalshehu <amalshehu@gmail.com>
2017-07-19 11:51:19 +0530

    Subscribe data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AppService } from './app.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  result: any;
   title = 'app';
   searchInput: string;
+  searchUrl = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  constructor(public service: AppService) {}
+  performSearch() {
+    const apiLink = this.searchUrl + this.searchInput + '&limit=5';
+    this.service.search(apiLink).subscribe(
+      (data: any) => this.result = data
+    )
+  }
+
 }
